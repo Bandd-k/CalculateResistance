bca6f2f4060b05a837c9dbc772c470e1cc978fee
Denis <banddk1@gmail.com>
2015-02-15 13:39:18 +0300

    Add documentation

--- FloydWarhall.py
+++ FloydWarhall.py
@@ -2,13 +2,15 @@ import xml
 from xml.etree import ElementTree
 import time
 import sys
-def div(a,b):
+
+
+def div(a, b):
     try:
-        c=a/b
+        c = a / b
     except ZeroDivisionError:
-        c=float("inf")
+        c = float("inf")
     return c
-    
+
 
 ElementTypes = ['resistor', 'capactor', 'diode']
 try:
@@ -27,21 +29,21 @@ for x in ElementTypes:
     y = tree.findall('.//' + x)
     for z in y:
         t = z.attrib
-        b[int(t['net_from']) - 1][int(t['net_to']) - 1] = div(1,(
-            div(1,b[int(t['net_from']) - 1][int(t['net_to']) - 1]) + div(1,float(t['resistance']))))
+        b[int(t['net_from']) - 1][int(t['net_to']) - 1] = div(1, (
+            div(1, b[int(t['net_from']) - 1][int(t['net_to']) - 1]) + div(1, float(t['resistance']))))
         if x == 'diode':
-            b[int(t['net_to']) - 1][int(t['net_from']) - 1] = div(1,(
-                div(1,b[int(t['net_to']) - 1][int(t['net_from']) - 1]) + div(1,float(t['reverse_resistance']))))
+            b[int(t['net_to']) - 1][int(t['net_from']) - 1] = div(1, (
+                div(1, b[int(t['net_to']) - 1][int(t['net_from']) - 1]) + div(1, float(t['reverse_resistance']))))
         else:
-            b[int(t['net_to']) - 1][int(t['net_from']) - 1] = div(1,(
-                div(1,b[int(t['net_to']) - 1][int(t['net_from']) - 1]) + div(1,float(t['resistance']))))
+            b[int(t['net_to']) - 1][int(t['net_from']) - 1] = div(1, (
+                div(1, b[int(t['net_to']) - 1][int(t['net_from']) - 1]) + div(1, float(t['resistance']))))
 
             # last part of the alorithm
             # start
 for k in range(n):
     for i in range(n):
         for j in range(n):
-            b[i][j] = div(1,(div(1,b[i][j]) + div(1,(b[i][k] + b[k][j]))))
+            b[i][j] = div(1, (div(1, b[i][j]) + div(1, (b[i][k] + b[k][j]))))
 file = open(sys.argv[2], 'w')
 for i in range(n):
     if i > 0:
@@ -50,4 +52,3 @@ for i in range(n):
         file.write(str(round(b[i][j], 7)) + ',')
 file.close()
 print(time.clock() - tm)
-
